8626b3435fd3e26483de8451b1fb034a3c9ebce8
Palladiusz <palladiusz.ra@gmail.com>
2020-01-05 11:27:10 +0100

    Database in mongodb is up

---
+++ server/database.js
@@ -0,0 +1,22 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+mongoose.connect('mongodb://localhost:27017/vexDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
+
